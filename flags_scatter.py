64f9700b79120b4628d70a86d986b031c91d07eb
Lars Holm <s153920@student.dtu.dk>
2020-02-26 21:07:31 +0100

    scatter minor update

--- flags_scatter.py
+++ flags_scatter.py
@@ -18,16 +18,16 @@ import matplotlib.pyplot as plt
 ## Scatterplot colored by landmass
 
 
-# Landmass names
-LAMANames = ['N. America','S. America','Europe','Africa','Asia','Oceania']
+# INPUT
+catNames = ['N. America','S. America','Europe','Africa','Asia','Oceania']
+variable = 'LAMA'
 
-
-attributeNames_c = attributeNames.copy();
-
+xVariable = 'AREA'
+yVariable = 'POPU'
 
 # Finds the columns of area and population
-i = np.where(attributeNames=='AREA')[0][0]
-j = np.where(attributeNames=='POPU')[0][0]
+i = np.where(attributeNames==xVariable)[0][0]
+j = np.where(attributeNames==yVariable)[0][0]
 
 
 # Defines the used colors in legens
@@ -35,10 +35,10 @@ color = ['r','g', 'b', 'c', 'm', 'y']
 
 ## Begin plot
 plt.title('Area and population, colored by landmass')
-for c in range(len(LAMANames)):
+for c in range(len(catNames)):
     
     # Makes a variable for every c which is used to find the specific columns
-    idx = 'LAMA' + str(c)
+    idx = variable + str(c)
     
     # The column for each landmass inclued 0's and 1's, which are multiplied for
     # each loop to the columsn for area and population, respectively
@@ -46,10 +46,10 @@ for c in range(len(LAMANames)):
                 y=X[:, np.where(attributeNames==idx)[0][0]]*X[:,j], 
                 c=color[c], 
                 s=50, alpha=0.5,
-                label=LAMANames[c])
+                label=catNames[c])
 plt.legend()
-plt.xlabel(attributeNames_c[i])
-plt.ylabel(attributeNames_c[j])
+plt.xlabel(attributeNames[i])
+plt.ylabel(attributeNames[j])
 plt.show()
 
 
