a1a16600de3c86bf4287b41784377d4d29685d17
Lars Holm <s153920@student.dtu.dk>
2020-02-27 09:40:11 +0100

    updated basic boxplot and histogram

--- flags_boxplot.py
+++ flags_boxplot.py
@@ -9,11 +9,18 @@ Created on Thu Feb 27 09:04:54 2020
 from flags_load_data import X, attributeNames
 import seaborn as sns
 import numpy as np
+import matplotlib.pyplot as plt
 
-variable = 'POPU'
+variable = 'COLO'
 
 df = X[:, np.where(attributeNames==variable)[0][0]]
  
 # Make boxplot for one group only
 sns.boxplot( y=df )
+
+plt.title('Number of colors in a flag')
+plt.ylabel('# colors in flags')
+
+
+
 #sns.plt.show()
--- flags_histogram.py
+++ flags_histogram.py
@@ -9,13 +9,23 @@ Created on Thu Feb 27 09:11:33 2020
 from flags_load_data import X, attributeNames
 import seaborn as sns
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 variable = 'COLO'
 
 df = X[:, np.where(attributeNames==variable)[0][0]]
  
 # Make default histogram    
-sns.distplot( df )
+sns.distplot( df , kde=False)
+
+plt.title('Number of colors in a flag')
+plt.xlabel('# colors in flags')
+plt.ylabel('Values')
+
+
+
+
 #sns.plt.show()
  
 # Control the number of bins
