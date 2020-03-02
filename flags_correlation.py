93ab7eb21ed91416412377cd56c4fd9ae4ee09bb
Lars Holm <s153920@student.dtu.dk>
2020-03-02 10:12:35 +0100

    Added comments and cleaned script

--- flags_correlation.py
+++ flags_correlation.py
@@ -5,13 +5,16 @@ Created on Mon Mar  2 08:13:04 2020
 @author: larsh
 """
 
+# Script for generating correlation matrix
+
+# Libraries
 from pandas import DataFrame
 import numpy as np
 from flags_load_data import X, attributeNames
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-# Dette gøres for at ændre matrixens værdiger fra ojekter til float
+# Change data matrix from object to float type
 N = len(X)
 M = len(X[0])
 X_float = np.zeros((len(X), len(X[0])))
@@ -20,9 +23,10 @@ for i in range(0, len(X)):
     for j in range(0, len(X[0])):
         X_float[i,j] = X[i,j]
 
-# Create zero vector
+# Create empty dictionary list
 Data = {}
 
+# Every attributes and corresponding values are inserted as a dictionary in the data list
 for c in range(len(X[0])):
     
     variable = attributeNames[c]
@@ -30,23 +34,14 @@ for c in range(len(X[0])):
     Data[variable] = X_float[:,index]
 
 
-
+# Dataframe is created 
 df = DataFrame(Data,columns=attributeNames)
 
-
+# Correlation matrix is created
 corrMatrix = df.corr()
 print (corrMatrix)
 
+# Generating the heatmap (correlation matrix)
 sn.set(font_scale=2)
 fig, ax = plt.subplots(figsize=(30,30))
-sn.heatmap(corrMatrix, annot=False, linewidths=.5, ax=ax)
-
-
-
-# Old:
-#Data = {'A': X_float[:,np.where(attributeNames=='AREA')[0][0]],
-#        'B': X_float[:,np.where(attributeNames=='POPU')[0][0]],
-#        'C': X_float[:,np.where(attributeNames=='SUNS')[0][0]]
-#        }
-
-#df = DataFrame(Data,columns=['A','B','C'])
+sn.heatmap(corrMatrix, annot=False, linewidths=.5, ax=ax, vmin=-1,vmax=1, cmap="RdBu")
