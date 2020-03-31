62ebf50827c7f2ee16921b138bdd1e0edac8a640
Lars Holm <s153920@student.dtu.dk>
2020-03-31 11:30:01 +0200

    small changes

--- flags_reg.py
+++ flags_reg.py
@@ -19,13 +19,8 @@ attributeNames = np.delete(attributeNames,var)
 y = Xstand[:,var]
 
 # Sletter følgende kolonner, da denne giver en singular matrix
-slettes = ['BOTR0','BOTR1','BOTR2','BOTR3','BOTR4','BOTR5','BOTR6','BOTR7']
-#slettes = ['BOTR0','BOTR1','BOTR2','BOTR3','BOTR4','BOTR5','BOTR6','BOTR7','TOPL0', 'TOPL1',
-#       'TOPL3', 'TOPL4', 'TOPL5', 'TOPL6', 'TOPL7','MAIN0', 'MAIN1', 'MAIN2', 'MAIN3',
-#       'MAIN4', 'MAIN5', 'MAIN6', 'MAIN7','LAMA0', 'LAMA1', 'LAMA2', 'LAMA3', 'LAMA4', 'LAMA5',
- #      'ZONE0', 'ZONE1', 'ZONE2', 'ZONE3','TEXT','LANG0', 'LANG1', 'LANG2', 'LANG3', 'LANG4',
-#       'LANG5', 'LANG6', 'LANG7', 'LANG8', 'LANG9', 'RELI0', 'RELI1',
-#       'RELI2', 'RELI3', 'RELI4', 'RELI5', 'RELI6', 'RELI7','BARS','ORAN','BLAC','ICON']
+slettes = ['BOTR3','BOTR5']
+
 for z in range(len(slettes)):
     o = slettes[z]
     var = np.where(attributeNames==o)[0][0]
@@ -70,7 +65,7 @@ for train_index, test_index in CV.split(X,y):
     y_train = y[train_index]
     X_test = X[test_index]
     y_test = y[test_index]
-    internal_cross_validation = 10    
+   # internal_cross_validation = 10    
 
     # Standardize outer fold based on training set, and save the mean and standard
     # deviations since they're part of the model (they would be needed for
@@ -87,27 +82,29 @@ for train_index, test_index in CV.split(X,y):
     XtX = X_train.T @ X_train
     
     # Compute mean squared error without using the input data at all
-    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
-    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
+    #Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
+    #Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
 
 
     for i in range(len(lambdas)): 
         l = lambdas[i]
-        # Estimate weights for the optimal value of lambda, on entire training set
+        # Estimate weights for the value of lambda, on entire training set
         lambdaI = l * np.eye(M)
         lambdaI[0,0] = 0 # Do no regularize the bias term
         w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze() ##################################
-        # Compute mean squared error with regularization with optimal lambda
+        # Compute mean squared error with regularization with lambda
         Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
         Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
     
         # Estimate weights for unregularized linear regression, on entire training set
-        w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
+        #w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
         # Compute mean squared error without regularization
-        Error_train[i,k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
-        Error_test[i,k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
+        #Error_train[i,k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
+        #Error_test[i,k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
       
-print(np.sum(sigma<0.001))
+    k=k+1
+    
+print(np.sum(sigma<0.01))
 
 # Mangler at finde middelværdi af Error_train og Error_split for hver kolonne.
 # Dette angiver error ved hver lambda værdi  valgte interval
